6202e76a1e397cb69a40aea7da014bb646b04b6f
schmidte10 <elliott.a.schmidt@gmail.com>
2023-08-01 09:00:51 +1000

    added loop to take mean of three last slopes for background resp.
    script should be better organised and labelled in the futre but is working.
    
    Measures the highest rate within a slope with an rsq = 0.95 and substracts the background of the three last slopes

--- R_files/respR_maximum_metabolic_rate.R
+++ R_files/respR_maximum_metabolic_rate.R
@@ -17,10 +17,12 @@ setwd("C:/Users/jc527762/OneDrive - James Cook University/PhD dissertation/Data/
 #--- fish data ---# 
 FISH_ID = "133.1"
 mass = 0.0007676 #in kilograms 
-chamber = "ch1" 
+chamber = "ch4" 
 temperature = 28.5 
 salinity = 35 
 chamber_vol = 0.04860
+system1 = "DELL" 
+Date1 = "01/07/2023"
 
 #--- import data ---# 
 firesting <- read_delim("C:/Users/jc527762/OneDrive - James Cook University/PhD dissertation/Data/2023/Resp_backup/2023_Resp/Dell7440/Experiment_ 01 July 2023 11 56AM/Oxygen data raw/firesting.txt", 
@@ -57,9 +59,9 @@ cycle1.start = cycle1[1,1]
 cycle1.end = tail(cycle1, n=1)[1,1]
 
 #- find out which row in the firesting file has the same time as the cycle start time -# 
-cycle1.start.row <- which(firesting2$TIME == cycle1.start); cycle.start.row
+cycle1.start.row <- which(firesting2$TIME == cycle1.start); cycle1.start.row
 #- find out which row in the firesting file has the same time as the cycle end time -#
-cycle1.end.row <- which(firesting2$TIME == cycle1.end); cycle.end.row
+cycle1.end.row <- which(firesting2$TIME == cycle1.end); cycle1.end.row
 
 #- subset the first cycle from the firesting file -#
 cycle1_data <- firesting2 %>% 
@@ -93,7 +95,8 @@ mmr2 <- mmr %>%
                volume = chamber_vol,
                mass = mass,
                S = salinity, 
-               t = temperature)
+               t = temperature, 
+               P = 1.013253)
   
 summary(mmr2)
 
@@ -108,6 +111,10 @@ FISH_ID;mmr_final[1,29];mmr_final[1,29]*mass
 #--- background rates ---# 
 pre1 <- read.csv("C:/Users/jc527762/OneDrive - James Cook University/PhD dissertation/Data/2023/Resp_backup/2023_Resp/Dell7440/Experiment_ 01 July 2023 11 15AM/All slopes/Cycle_2.txt", 
                    sep = ";")
+pre2 <- read.csv("C:/Users/jc527762/OneDrive - James Cook University/PhD dissertation/Data/2023/Resp_backup/2023_Resp/Dell7440/Experiment_ 01 July 2023 11 15AM/All slopes/Cycle_3.txt", 
+                 sep = ";")
+pre3 <- read.csv("C:/Users/jc527762/OneDrive - James Cook University/PhD dissertation/Data/2023/Resp_backup/2023_Resp/Dell7440/Experiment_ 01 July 2023 11 15AM/All slopes/Cycle_4.txt", 
+                 sep = ";")
 
 firesting_pre <- read_delim("C:/Users/jc527762/OneDrive - James Cook University/PhD dissertation/Data/2023/Resp_backup/2023_Resp/Dell7440/Experiment_ 01 July 2023 11 15AM/Oxygen data raw/firesting.txt", 
                            delim = "\t", escape_double = FALSE, 
@@ -129,8 +136,31 @@ firesting_pre2 <-  firesting %>%
          TIME = as.character(TIME)) %>% 
   inspect()
 
+cycle_list <- list(pre1,pre2,pre3)
+bg <- list()
 
-pre1 <- pre1 %>% 
+for (i in 1:3) {
+  bg[[i]] <- cycle_list[[i]] %>% 
+    select(c(1:2,4:7))  %>% 
+    rename(TIME = Time, 
+           dTIME = Seconds.from.start.for.linreg,  
+           ch1 = ch1.po2, 
+           ch2 = ch2.po2,
+           ch3 = ch3.po2, 
+           ch4 = ch4.po2) %>% 
+    select(c("dTIME", all_of(chamber),"TIME")) %>% 
+    rename(Oxygen = chamber) %>% 
+    mutate(dTIME = as.numeric(dTIME), 
+           Oxygen = as.numeric(Oxygen), 
+           TIME = as.character(TIME)) %>% 
+    inspect() %>% 
+    calc_rate.bg()
+}
+
+
+pre_final[[1]]$rate.bg;pre_final[[2]]$rate.bg;pre_final[[3]]$rate.bg
+
+pre1_final <- pre1 %>% 
   select(c(1:2,4:7)) %>% 
   rename(TIME = Time, 
          dTIME = Seconds.from.start.for.linreg,  
@@ -142,7 +172,9 @@ pre1 <- pre1 %>%
   rename(Oxygen = chamber) %>% 
   mutate(dTIME = as.numeric(dTIME), 
          Oxygen = as.numeric(Oxygen), 
-         TIME = as.character(TIME))
+         TIME = as.character(TIME)) %>% 
+  inspect() %>% 
+  calc_rate.bg()
 
 
 bg_pre <- inspect(pre1, dtime = 1, oxygen=2) |> 
@@ -150,8 +182,8 @@ bg_pre <- inspect(pre1, dtime = 1, oxygen=2) |>
   summary()
 
 
-mmr_adj <- adjust_rate(mmr, by=bg_pre, method = "value")
-head(summary(mmr_adj))
+mmr_adj <- adjust_rate(mmr, by=c(bg[[1]]$rate.bg,bg[[2]]$rate.bg,bg[[3]]$rate.bg), method = "mean")
+mmr_adj
 
 mmr_adj2 <- mmr_adj %>% 
   convert_rate(oxy.unit = "%Air", 
@@ -160,7 +192,8 @@ mmr_adj2 <- mmr_adj %>%
                volume = chamber_vol,
                mass = mass,
                S = salinity, 
-               t = temperature)
+               t = temperature, 
+               P = 1.013253)
 
 summary(mmr_adj2)
 
@@ -170,6 +203,19 @@ mmr_final <- mmr_adj2 %>%
   plot(type = "full") %>%
   summary(export = TRUE)
 
-FISH_ID;mmr_final[1,29];mmr_final[1,29]*mass
+FISH_ID;mmr_final$rate.output;mmr_final$rate.output*mass
 
+`mmr(mg/h/kg)` <- mmr_final$rate.output *-1
+mmr <- mmr_final$rate.output*mass * -1
 
+results <- data.frame(FISH_ID = FISH_ID, 
+                      Mass = mass, 
+                      Chamber = chamber, 
+                      System = system1,
+                      Volume = chamber_vol, 
+                      Date = Date1,
+                      Salinity = salinity, 
+                      Temperature = temperature, 
+                      `mmr(mg/h/kg)` = `mmr(mg/h/kg)`, 
+                      mmr = mmr) %>% 
+  view()
