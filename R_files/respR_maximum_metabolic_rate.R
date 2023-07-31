83998d3724fcad25ce8d9ff884df14fb1f250f8e
schmidte10 <elliott.a.schmidt@gmail.com>
2023-07-31 15:45:30 +1000

    start of importing cycle data

--- R_files/respR_maximum_metabolic_rate.R
+++ R_files/respR_maximum_metabolic_rate.R
@@ -16,14 +16,16 @@ library("data.table")
 #--- fish data ---# 
 FISH_ID = "example1"
 mass = 0.0007676 #in kilograms 
-chamber = "ch1" 
+chamber = "ch4" 
 tempearture = 28.5 
 
 #--- import data ---# 
-firesting <- read_delim("C:/Users/jc527762/OneDrive - James Cook University/PhD dissertation/Data/2023/Resp_backup/2023_Resp/Dell7440/Experiment_ 01 July 2023 11 15AM/Oxygen data raw/firesting.txt", 
+firesting <- read_delim("C:/Users/jc527762/OneDrive - James Cook University/PhD dissertation/Data/2023/Resp_backup/2023_Resp/Dell7440/Experiment_ 01 July 2023 11 56AM/Oxygen data raw/firesting.txt", 
                              delim = "\t", escape_double = FALSE, 
                              col_types = cols(`Time (HH:MM:SS)` = col_time(format = "%H:%M:%S")), 
                              trim_ws = TRUE, skip = 19)
+
+#--- data manipulation ---#
 firesting2 <- firesting %>% 
   select(c(1:3,5:8)) %>% 
   rename(TIME = `Time (HH:MM:SS)`, 
@@ -34,7 +36,12 @@ firesting2 <- firesting %>%
          ch3 = Ch3...7, 
          ch4 = Ch4...8) %>% 
   select(c("dTIME", all_of(chamber),"TIME")) %>% 
-  rename(Oxygen = chamber) 
+  rename(Oxygen = chamber) %>% 
+  mutate(dTIME = as.numeric(dTIME), 
+         Oxygen = as.numeric(Oxygen))
 
+#--- inspect file ---#
+ap <- inspect(firesting2)
 
+#--- import cycle data ---# 
 
